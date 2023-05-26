c03ed4228811fdcdae1f00660b0f8ecc9ea291d5
kshim-personal-notebook-window <a41533503@gmail.com>
2023-05-26 22:54:43 +0900

    working on

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -8,6 +8,8 @@
 #include <ctime>
 #include <numeric>
 
+
+// resize와 reserve의 기능상 차이점
 Span::Span(unsigned int num)
 {
 	this->N_ = num;
@@ -48,34 +50,25 @@ void	Span::addNumber(unsigned int num)
 	this->stored_.push_back(num);
 }
 
-// iterator에 증감 연산하는게 올바른 동작인가?
-	// 적절한 algorithm이 안보여서 이렇게 만들었는데, 혹시 다른 방법이 있을까?
-	// iterator는 증가연산을 구현한다. 따라서 증가 연산을 해도 내부에서 구현된 동작이 수행될 것으로 보인다.
-	// !!! 단, 감소 연산은 그렇지 않은 것 같다
-		// reverse iterator 사용하기
 unsigned int Span::shortestSpan() const
 {
-	std::vector<int>::const_iterator	start = this->stored_.begin();
-	std::vector<int>::const_iterator 	end = this->stored_.end();
-	std::vector<int>					tmp(5);
+	std::vector<int>					tmp(this->stored_);
 
 	if (this->N_ <= 1)
 		throw std::invalid_argument("one or no numbers stored");
-	std::adjacent_difference(start, end, tmp);
-	// tmp에서 제일 작은 것 찾기
+	std::adjacent_difference(std::begin(tmp), std::end(tmp), std::begin(tmp));
+	// tmp에서 제일 작은 것 찾기 (절대값 고려)
 	return (span);
 }
 
 unsigned int Span::longestSpan() const
 {
-	std::vector<int>::const_iterator	start = this->stored_.begin();
-	std::vector<int>::const_iterator 	end = this->stored_.end();
-	std::vector<int>					tmp(5);
+	std::vector<int>					tmp(this->stored_);
 
 	if (this->N_ <= 1)
 		throw std::invalid_argument("one or no numbers stored");
-	std::adjacent_difference(start, end, tmp);
-	// tmp에서 제일 작은 것 찾기
+	std::adjacent_difference(std::begin(tmp), std::end(tmp), std::begin(tmp));
+	// tmp에서 제일 큰 것 찾기 (절대값 고려)
 	return (span);
 }
 
